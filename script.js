e27afa6d659d4917bd5e74dcf36840e561f897f9
myuser <myemail>
2024-03-17 20:13:16 -0400

    building the scenery
    add fly
    add sound

--- script.js
+++ script.js
@@ -20,9 +20,39 @@ var bX = 33;
 var bY = 200;
 var gravity = 1.4;
 var score = 0;
+var pipe = [];
+
+pipe[0] = {
+    x : canvas.clientWidth,
+    y : 0
+}
 
 // upload audio
 var fly = new  Audio();
 fly.src = "sounds/fly.mp3"
 var scoreAudio = new  Audio();
 scoreAudio.src = "sounds/score.mp3"
+
+ //print Screen
+ document.addEventListener("keydown",flying);
+ //flying
+ function flying(){
+    bY = bY - 26;
+    fly.play();
+ }
+
+function jogo(){
+    //background game
+    ctx.drawImage(bg,0,0) //drawImage (image,x,y)
+
+    //drawing floor
+    ctx.drawImage(floor , 0 ,canvas.height - floor.height);
+
+    //drawing bird
+    ctx.drawImage(bird ,bX ,bY );
+    bY += gravity;
+
+    requestAnimationFrame(jogo);
+}
+
+jogo();
